404c7249e21e67006552964cebc342d17e76bdc9
duck2913 <minhducdl1010@gmail.com>
2021-12-10 09:29:16 +0100

    update js

---
+++ .history/main.77bb5cfd_20211210083842.js
@@ -0,0 +1,5 @@
+const btnSubmit = document.querySelector('.btn--submit');
+
+btnSubmit.addEventListener('click', function () {
+    alert('Thank you for your registration')
+})
---
+++ .history/main.77bb5cfd_20211210084044.js
@@ -0,0 +1,6 @@
+const btnSubmit = document.querySelector('.btn--submit');
+console.log("🚀 -> btnSubmit", btnSubmit);
+
+btnSubmit.addEventListener('click', function () {
+    alert('Thank you for your registration');
+})
